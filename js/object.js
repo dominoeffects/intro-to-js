774e1ddcf68c5b137b289765d332d58c6decf720
Dominique Maez <Domino@Dominiques-MacBook-Pro-2.local>
2019-04-23 20:56:53 -0700

    practicing with object data in object.js

---
+++ js/object.js
@@ -0,0 +1,31 @@
+// Object data type
+var obj = {}; // Empty object
+var car = {
+    make: 'honda', 
+    model: 'civic',
+    year: 2019
+};
+
+console.log(car);
+console.log(car.model);
+console.log(car.year);
+
+
+var date = {
+    //add date, month, year, day property with todays date
+    date: 23,
+    month: 4,
+    // month 'April',
+    year: 2019,
+    day: 'Tuesday'
+}
+
+//display Tuesday on console
+
+console.log(date.day);
+
+var userCred = {
+    username: 'dommaez',
+    password: 'password1'
+};
+console.log(userCred);
